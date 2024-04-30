6b8859ef58a045140071aa47b3e9560e5e6f23cf
sarth830 <sarthjain830@gmail.com>
2024-04-30 09:52:15 -0400

    Update dev.js

--- config /dev.js
+++ config /dev.js
@@ -1,12 +1,12 @@
 // module.exports={
 //
 //     JWT_SECRET:"jefojwlfje",
-//     DATABASE:"mongodb+srv://sarthakStore:sMZWtejya3rWteQ@cluster0.arnfk.mongodb.net/CocoberryCraftStore?retryWrites=true&w=majority",
+//     DATABASE:"mongodb+srv://sarthakStore:sa@cluster0.arnfk.mongodb.net/CocoberryCraftStore?retryWrites=true&w=majority",
 //     NODE_MAILER_EMAIL:"noreplymykindainsta@gmail.com",
-//     PASSWORD:"Mykindainsta9993",
+//     PASSWORD:"abc",
 //     EMAIL:"sarthjain830@gmail.com",
 //     JWT_RESET_PASSWORD:"secret",
 //     CLIENT_URL:"http://localhost:3000",
-//     GOOGLE_CLIENT_ID:"437349581282-tndkqm60j0k0qrr07j9j5117tmii30ua.apps.googleusercontent.com"
+//     GOOGLE_CLIENT_ID:"as-tndkqm60j0k0qrr07j9j5117tmii30ua.apps.googleusercontent.com"
 //
 // }
